e3482b90973dfac5491329006ea5c3a56a3529af
Shahani Callueng <100544332+shahanic@users.noreply.github.com>
2024-04-29 11:55:37 +0800

    Login and Sign up with database

---
+++ app/src/main/java/com/example/finalprojectse/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.finalprojectse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    }
